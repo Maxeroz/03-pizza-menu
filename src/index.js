499b2311b20994d4aa5edf0903fcb1dc4396e21f
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-12-09 23:26:18 +0400

    Conditional Rendering With Ternaries

--- src/index.js
+++ src/index.js
@@ -69,20 +69,22 @@ function Header() {
 }
 
 function Menu() {
-  // const pizzas = pizzaData;
-  const pizzas = [];
+  const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObg={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -112,11 +114,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
@@ -146,3 +152,4 @@ root.render(
 // The Rules of JSX
 // Rendering Lists
 // Conditional Rendering With &&
+// Conditional Rendering With Ternaries
