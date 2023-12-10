d9894e42afe57f28e9c0eecf20c5e89bf3e2188b
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-12-10 22:16:20 +0400

    Extracting JSX Into a New Component

--- src/index.js
+++ src/index.js
@@ -118,10 +118,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -130,6 +127,17 @@ function Footer() {
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 // React v18
@@ -157,3 +165,4 @@ root.render(
 // Conditional Rendering With &&
 // Conditional Rendering With Ternaries
 // Conditional Rendering With Multiple Returns
+// Extracting JSX Into a New Component
